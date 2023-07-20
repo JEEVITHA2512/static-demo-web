7b39c092f18fb0beb63b71e58290c2cda02ab94a
linux <jeevithamurugan.2512@gmail.com>
2023-07-20 11:20:30 +0530

    requirements.txt

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Add an event listener to the header to change the background color on click
+const header = document.querySelector('header');
+header.addEventListener('click', function() {
+  header.style.backgroundColor = 'blue';
+});
